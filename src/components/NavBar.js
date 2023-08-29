f6138b87d48353e7767581258e240b39b264b021
Shallon <91329012+smsharon@users.noreply.github.com>
2023-08-28 20:14:20 -0700

    add and style navbar (#12)

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,52 @@
-import React from "react";
-function NavBar(){
-    return(
-        <></>
-    )
+import React, { useState } from 'react';
+import './NavBar.css'; 
 
+function NavBar() {
+  const [isAccountDropdownOpen, setIsAccountDropdownOpen] = useState(false);
+
+  const toggleAccountDropdown = () => {
+    setIsAccountDropdownOpen(!isAccountDropdownOpen);
+  };
+
+  return (
+    <header className="navbar">
+      <div className="logo">Online Store</div>
+      <nav>
+        <ul className="nav-links">
+          <li>
+            <button>Home</button>
+          </li>
+          <li className="search">
+            <input type="text" placeholder="Search products..." />
+          </li>
+          <li>
+            <button>Cart</button>
+          </li>
+          <li className="account-dropdown">
+            <button onClick={toggleAccountDropdown}>My Account</button>
+            {isAccountDropdownOpen && (
+              <div className="dropdown-options">
+                <form>
+                  <label htmlFor="email">Email:</label>
+                  <input type="email" id="email" />
+                  <label htmlFor="password">Password:</label>
+                  <input type="password" id="password" />
+                  <button>Login</button>
+                </form>
+                <form>
+                  <label htmlFor="newEmail">Email:</label>
+                  <input type="email" id="newEmail" />
+                  <label htmlFor="newPassword">Password:</label>
+                  <input type="password" id="newPassword" />
+                  <button>Sign Up</button>
+                </form>
+              </div>
+            )}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
+
 export default NavBar;

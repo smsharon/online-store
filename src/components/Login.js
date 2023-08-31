2ed9fadce621d309d2c1f914a45680ec1b75c312
Shallon Mokeira Said <shallonmokeira@gmail.com>
2023-08-31 01:27:27 -0400

    feat:add product details, update login and registration functionality

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,56 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, {useState} from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate(); 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:8002/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Login successful, handle success scenario
+        navigate('/'); // Redirect to home page
+      } else {
+        setErrorMessage('Invalid email or password');
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+    }
+  };
+
   return (
     <div className="login-container">
       <div className='form-container'>
       <h2>Login</h2>
-      <form>
+      <form onSubmit={handleLogin}>
         <label htmlFor="email">Email:</label>
-        <input type="email" id="email" />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label htmlFor="password">Password:</label>
-        <input type="password" id="password" />
-        <button>Login</button>
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        <button type="submit">Login</button>
       </form>
       <p>
         No account? <Link to="/signup">Create account</Link>
